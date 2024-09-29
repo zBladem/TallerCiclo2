ff863f846c3e1320957c91dc5260a66c9ab12137
Mayhem <alexandersaca3@gmail.com>
2024-09-29 02:07:40 -0500

    Arco de animaciones 👻
    - Sistema de animación con blend trees añadido
    - Animaciones de caminata, correr y cansado quieto abajo añadido
    - Orden de carpetas
    - Ajustes en los códigos del jugador para las animaciones

--- Assets/Code/Scripts/Player/PlayerMovement.cs
+++ Assets/Code/Scripts/Player/PlayerMovement.cs
@@ -5,6 +5,7 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody2D rb;
     private PlayerStamina stamina;
     private Transform hideInteract;
+    private Animator animator;
     private HidingSystem hidingSystem;
 
     [Header("Movement")]
@@ -19,6 +20,7 @@ public class PlayerMovement : MonoBehaviour
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
         stamina = GetComponent<PlayerStamina>();
     }
     private void Start()
@@ -29,15 +31,15 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         Movement();
+        PlayerAnimations();
     }
     private void Movement()
     {
-        moveInput.x = Input.GetAxisRaw("Horizontal");
-        moveInput.y = Input.GetAxisRaw("Vertical");
+        moveInput.x = Input.GetAxisRaw("Horizontal"); moveInput.y = Input.GetAxisRaw("Vertical");
 
         moveInput.Normalize();
 
-        running = moveInput != Vector2.zero && Input.GetKey(KeyCode.LeftShift);
+        running = moveInput != Vector2.zero && Input.GetKey(KeyCode.LeftShift) && !stamina.Tired;
 
         rb.velocity = moveInput * currentSpeed;
 
@@ -48,5 +50,20 @@ public class PlayerMovement : MonoBehaviour
         else if (hidingSystem.IsHiding) currentSpeed = 0;
 
         else if (stamina.Tired) currentSpeed = tiredSpeed;
+    }
+    private void PlayerAnimations()
+    {
+        animator.SetBool("Running", running);
+        animator.SetBool("Tired", stamina.Tired);
+
+        if (!hidingSystem.IsHiding)
+        {
+            animator.SetFloat("Xaxis", moveInput.x); animator.SetFloat("Yaxis", moveInput.y);
+
+            if (moveInput != Vector2.zero)
+            {
+                animator.SetFloat("DirX", moveInput.x); animator.SetFloat("DirY", moveInput.y);
+            }
+        }
     }
 }
